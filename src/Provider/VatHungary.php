bc426bf1de82e30ff1baa998431daace86ad90cf
Ricardo Velhote <rvelhote@gmail.com>
2016-12-14 23:00:43 +0000

    Added Hungary Provider Skeleton

---
+++ src/Provider/VatHungary.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+namespace Welhott\Vatlidator\Provider;
+
+use Welhott\Vatlidator\VatProvider;
+
+/**
+ * Class VatHungary
+ * @package Welhott\Vatlidator\Provider
+ */
+class VatHungary extends VatProvider
+{
+    /**
+     * The ISO 3166-1 alpha-2 code that represents this country
+     * @var string
+     */
+    private $country = 'HU';
+
+    /**
+     * The abbreviation of the VAT number according the the country's language.
+     * @var string
+     */
+    private $abbreviation = 'ANUM';
+
+    /**
+     *
+     * @return bool True if the number is valid, false if it's not.
+     */
+    public function validate() : bool
+    {
+        return false;
+    }
+
+    /**
+     * Obtain the country code that represents this country.
+     * @return string An ISO 3166-1 alpha-2 code that represents this country.
+     */
+    public function getCountry() : string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAbbreviation() : string
+    {
+        return $this->abbreviation;
+    }
+}
